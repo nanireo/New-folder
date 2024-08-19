796e63ddf6d45b76ae739f83fa7e4be2ec91af8e
nanireo <61157090+nanireo@users.noreply.github.com>
2024-08-19 23:27:43 +0530

    hannabed

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const navLinks = document.querySelector('.nav-links');
 
-mobileMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    mobileMenu.addEventListener('click', function() {
+        mobileMenu.classList.toggle('open');
+        navLinks.classList.toggle('show');
+    });
 });
